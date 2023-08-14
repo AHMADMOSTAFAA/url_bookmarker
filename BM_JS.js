8a80bf513fc7fb060a9360e2f1a7bbe93d1bab7e
AHMADMOSTAFAA <ahmadmostafaa02@gmail.com>
2023-08-14 16:09:00 +0300

    second commit

--- BM_JS.js
+++ BM_JS.js
@@ -1,24 +1,23 @@
 var websites=[];
 var websitename=document.getElementById('bookmarkname');
 var websiteurl=document.getElementById('bookmarkurl');
-var dialog=``;
 if(localStorage.getItem('websitekey')!=null){
 websites= JSON.parse(localStorage.getItem('websitekey'))
 display()
 }
 function submit(){
+  norepeat()
   var website={
     website_name:bookmarkname.value,
     website_url:bookmarkurl.value
   }
   websites.push(website);
-  localStorage.setItem('websitekey',JSON.stringify(websites));
   if(urlregexp.test(website.website_url)===true){
+  localStorage.setItem('websitekey',JSON.stringify(websites));
 display()/*?*/
   }
-  if(urlregexp.test(website.website_url)===false) {
-    dialog+=`<dialog open class="d-flex shadow justify-content-center align-items-center position-absolute text-center w-75 flex-wrap fs-3 bg-white p-4 rounded-2"><p>The url you typed isnt valid please follow the rules below :<br></p><i class="fa-regular fa-circle-right p-2"></i><p>the url mst be like:http://www.example.com/index.html</p></dialog>`
-    document.getElementById('dialog').innerHTML=dialog
+else if(urlregexp.test(website.website_url)===false) {
+ modal()
   }
   clearInputs()
 }
@@ -46,4 +45,25 @@ function clearInputs(){
   bookmarkurl.value="";
 }
    var urlregexp= /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-/*how to make no repeat*/
+  function modal(){
+    var dialog=``;
+    dialog+=`<dialog open class="d-flex shadow justify-content-center align-items-center position-absolute text-center w-75 flex-wrap fs-3 bg-white p-4 rounded-2"><p>The url you typed isnt valid please follow the rules below :<br></p><i class="fa-regular fa-circle-right p-2"></i><p>the url mst be like:http://www.example.com/index.html</p></dialog>`
+    document.getElementById('dialog').innerHTML=dialog
+   }
+   function modal_1(){
+    var dialog=``;
+    dialog+=`<dialog open class="d-flex shadow justify-content-center align-items-center position-absolute text-center w-75 flex-wrap fs-3 bg-white p-4 rounded-2"><p>The url  is already typed</p></dialog>`;
+    document.getElementById('dialog').innerHTML=dialog
+   }
+   function norepeat(){
+    console.log("7asal")
+    for (var i=0;i<websites.length;i++){
+      if(bookmarkurl.value==websites[i].website_url){
+        modal_1();
+        bookmarkurl.value="";
+      }
+      else{
+        console.log('tmam')
+      }
+    }
+   }
